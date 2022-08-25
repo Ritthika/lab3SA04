6423a15d34cf4ab7c276b23e1303a5cc4c2a5b2d
DRTK <ritthikai2544@gmail.com>
2022-08-26 01:17:53 +0700

    doit part commit

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const doIt = () => {
+    console.log("Hello from console")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={doIt}>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+ container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
